7957dedbe9d2a4456c957c0165515542c7bcd54e
ubaldooc <132728617+ubaldooc@users.noreply.github.com>
2026-01-12 06:48:22 -0700

    FIX process.env mal implementado y VITE_API_URL
    escribi mal el uso de process, y la variable de conexion a la api debe tener el formato VITE para que vite la reconozca, debe ser VITE_API_URL. SOLUCIONADO

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.API_URL || 'http://localhost:3000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
 
 /**
  * Función auxiliar genérica para realizar llamadas a la API.
